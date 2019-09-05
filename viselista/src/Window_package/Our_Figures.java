b0313c72e3b2a6c10c8d75cc4037f36fb74f1d18
Golubkin_timofey <Golubkin.timofey@users.noreply.github.com>
2019-09-06 00:26:52 +0300

    Начал добавлять GUI
    Создал 2 файла Our_Figures и Window_for_visilitsa. Первый файл существует для создавания фигур(Пока на стадии разработки, в дальнейшем постараюсь от него избавится) и второй для создания окна игры.
    
    label - это сообщение или вывеска для в окне
    
    button - это кнопка, при которой совершается действие описанное во втором классе в этом же файле.

--- viselista/src/Window_package/Our_Figures.java
+++ viselista/src/Window_package/Our_Figures.java
@@ -4,14 +4,12 @@ public class Our_Figures {
     private Color color;
     private int type;
     private String text;
-    Our_Figures(Color color, int type, String text){
+    Our_Figures(Color color, int type, String text) {
         this.color = color;
         this.text = text;
         this.type = type;
     }
-    public void paint_our_figures(Graphics g){
-        g.getColor(color);
-        g.fill
-
-    }
+public Color getcolor(){
+        return color;
+}
 }
--- viselista/src/Window_package/Window_for_visilitsa.java
+++ viselista/src/Window_package/Window_for_visilitsa.java
@@ -1,12 +1,45 @@
 package Window_package;
-
+import java.awt.*;
 import javax.swing.*;
-public class Window_for_visilitsa extends JLayeredPane {
-public Window_for_visilitsa(){
+import java.awt.event.*;
 
-    super("Visilitsa");
-    setBounds(100,100,300,200);
-setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+public class Window_for_visilitsa extends JFrame {
+    private JLabel label = new JLabel("Слово");
+    private JTextField input = new JTextField("", 5);
+    private JButton check_button = new JButton("Проверить");
+    private Container container = this.getContentPane();
+    Our_Figures x;
 
-}
+    public Window_for_visilitsa() {
+        super("Visilitsa");
+        setBounds(100, 100, 250, 100);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        container.setLayout(new GridLayout(3, 2, 2, 2));
+        container.add(input);
+        check_button.addActionListener(new ButtonEventListener());
+        container.add(check_button);
+        container.add(label);
+    }
+
+    public void addOurFigures(Our_Figures x) {
+        this.x = x;
+    }
+
+    /*@Override
+    public void paint(Graphics g) {
+        g.setColor(x.getcolor());
+        g.fillRect(100, 100, 100, 20);
+    }
+*/
+
+    class ButtonEventListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            label.setText(input.getText());
+            String message = "";
+            message += "Working";
+            message += "Text is" + label.getText();
+            JOptionPane.showMessageDialog(null, message, "Output", JOptionPane.PLAIN_MESSAGE);
+        }
+    }
 }
