7ebb8b068749d0e8fe93cc462554a244568e888c
Artem <tumanyan2000@yandex.ru>
2019-09-06 18:35:41 +0300

    НУжны изменения в методе Game

--- viselista/src/Main.java
+++ viselista/src/Main.java
@@ -1,83 +1,74 @@
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println("1st Player write your word!");
-        Scanner in = new Scanner(System.in); //инициализация Scanner'а
-        String word = in.next(); //ввод загаданного слова
-        String s_word = ""; //создание переменной, которая выводит секретное слово с "-" вместо букв
-        s_word += word.charAt(0); //добавление первой буквы слова
-        for (int i = 1; i < word.length()-1; i++)
-        {
-            if (word.charAt(i) == word.charAt(0)) //проверка на схожесть с первым символом
-                s_word += word.charAt(i);
-            else if (word.charAt(i) == word.charAt(word.length()-1)) //проверка на схожесть с последним символом
-                s_word += word.charAt(i);
-            else {
-                s_word += "-"; //заполнение центра слова "-"
-            }
-        }
-        s_word += word.charAt(word.length()-1); //добавление последней буквы слова
-        System.out.println(s_word); //вывод того, что получилось: истребитель -> и-----и---ь
-        System.out.println("2nd Player try to guess one of character!");
-        int flag; //флаг для проверки, угадал ли игрок букву
-        int steps = 0; //счётчик количества жизней
-        String first_str;
-        String second_str;
-        while (word.equals(s_word)!=true) //цикл самой игры
-        {
-            flag = 1; //подготовка флага
-            System.out.println("Write your character.");
-            char character  = in.next().charAt(0); //считывание массива символов, состоящий из 1 символа
-            String symbol="" + character;
-            for (int i = 1; i < word.length() - 1; i++) //цикл проверки - правильный ли символ
-            {
-                System.out.println("Значение переменной "+i);
-                 first_str =""+s_word.charAt(i);
-                 second_str=""+word.charAt(i);
-                if (first_str.equalsIgnoreCase(symbol)) //проверка на то, что такого же символа нет
-                {
+    private String word; // Загаданное слово
+    private String secretWord=""; // Секртеное слово
+    private int guesses; // текущее количество ходов
+    public static final int MAX_WRONG = 10; // Максимальное количество ошибок
+
+    public static void main(String[] args) {
+        Main game_1 = new Main();
+        game_1.setWord();
+        game_1.setSecretWord();
+        game_1.getSecretWord();
+        game_1.Game(game_1);
+        System.exit(0);
+    }
+
+    public void Game(Main object) {
+        int marker = 1;
+        char[] char_Secret_Word = object.getSecretWord().toCharArray();
+        char[] char_word = object.getWord().toCharArray();
+        while (Arrays.equals(char_word,char_Secret_Word) || (object.getGuesses() < MAX_WRONG)) {
+            System.out.println("Please,input character");
+            Scanner input = new Scanner(System.in);
+            char chr = input.next().toUpperCase().charAt(0);
+            for(int count = 0;count<word.length();count++) {
+                if (char_Secret_Word[count]==chr ) {
                     System.out.println("This symbol already exists!");
-                    flag =1;
-                    break;
-                } else if (second_str.equalsIgnoreCase(symbol)) { //проверка на то, что этот символ есть в изначальном слове
-                    s_word="";
-                    for (int k = 1; k < word.length()-1; k++)
-                    {
-                        if (word.charAt(k) == word.charAt(0)) //проверка на схожесть с первым символом
-                            s_word += word.charAt(k);
-                        else if (word.charAt(k) == word.charAt(word.length()-1)) //проверка на схожесть с последним символом
-                            s_word += word.charAt(k);
-                        else if(word.charAt(k)==character)
-                            s_word+=word.charAt(k);
-                        else {
-                            s_word += "-"; //заполнение центра слова "-"
-                        }
-                    }
-                    flag = 0;
+                    marker = 1;
+                } else if (char_word[count]==chr) {
+                    char_Secret_Word[count] = char_word[count];
+                    marker = 2;
                 }
             }
-            if (flag == 1) //проверка флага
-            {
-                steps += 1; //приближение к проигрышу
-                System.out.println("Oh no! You on " + steps + "/10 to death");
-            } else if (flag == 0) {
-                System.out.println("Nice!" + " Now your progress is "+s_word);
-            }
-            if (steps == 10)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    Thread.sleep(500); //создаёт задержку в 1 секунду, сделанно для эффекта
-                    System.out.println(".");
-                }
-                System.out.println("Omae wa mou shindeiru!!");
-                Thread.sleep(200);
-                System.exit(0); //завершение игры
+            if (marker == 1) {
+                ++guesses;
+                System.out.println("Guess "+(object.getGuesses()+"/10"));
+            } else if (marker == 2) {
+                System.out.println(char_Secret_Word);
             }
         }
-        System.out.println("Congratulations 2nd Player! You win!!");
+        System.out.println("Tnank you for game!");
+        return;
+    }
+
+
+    public void setWord() {
+        System.out.println("Please,input word");
+        Scanner scan = new Scanner(System.in);
+        this.word = scan.next();
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setSecretWord() {
+        int size = getWord().length();
+        for (int count = 0; count < size; count++)
+            secretWord += "-";
+    }
+
+
+    public String getSecretWord() {
+        return secretWord;
+    }
+
+    public int getGuesses() {
+        return guesses;
     }
 }
 
