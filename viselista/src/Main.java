5702434009e2cc17dd9bfb3317f1392e64211f3c
Artem <tumanyan2000@yandex.ru>
2019-09-06 11:38:55 +0300

    отказываемся от Char переходим к String

--- viselista/src/Main.java
+++ viselista/src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+
 import java.util.Scanner;
 
 public class Main {
@@ -19,27 +19,42 @@ public class Main {
             }
         }
         s_word += word.charAt(word.length()-1); //добавление последней буквы слова
-        String upgrade_s_word = s_word.toUpperCase() ;//Переводим первоначальную строку в верхний регистр
-        String upgrade_word  = word.toUpperCase();//Переводим первоначальную строку в верхний регистр
-        char[] s_word_chars = upgrade_s_word.toCharArray(); //преобразование строки в массивов чаров, для работы с символами по индексу
-        char[] word_chars = upgrade_word.toCharArray(); //преобразование строки в массивов чаров, для работы с символами по индексу
         System.out.println(s_word); //вывод того, что получилось: истребитель -> и-----и---ь
         System.out.println("2nd Player try to guess one of character!");
         int flag; //флаг для проверки, угадал ли игрок букву
         int steps = 0; //счётчик количества жизней
-        while (!Arrays.equals(word_chars, s_word_chars)) //цикл самой игры
+        String first_str;
+        String second_str;
+        while (word.equals(s_word)!=true) //цикл самой игры
         {
             flag = 1; //подготовка флага
             System.out.println("Write your character.");
-            char chr = in.next().toUpperCase().charAt(0); //считывание массива символов, состоящий из 1 символа
+            char character  = in.next().charAt(0); //считывание массива символов, состоящий из 1 символа
+            String symbol="" + character;
             for (int i = 1; i < word.length() - 1; i++) //цикл проверки - правильный ли символ
             {
-                if (s_word_chars[i] == chr) //проверка на то, что такого же символа нет
+                System.out.println("Значение переменной "+i);
+                 first_str =""+s_word.charAt(i);
+                 second_str=""+word.charAt(i);
+                if (first_str.equalsIgnoreCase(symbol)) //проверка на то, что такого же символа нет
                 {
                     System.out.println("This symbol already exists!");
-                    flag = 2;
-                } else if (word_chars[i] == chr) { //проверка на то, что этот символ есть в изначальном слове
-                    s_word_chars[i] = chr;
+                    flag =1;
+                    break;
+                } else if (second_str.equalsIgnoreCase(symbol)) { //проверка на то, что этот символ есть в изначальном слове
+                    s_word="";
+                    for (int k = 1; k < word.length()-1; k++)
+                    {
+                        if (word.charAt(k) == word.charAt(0)) //проверка на схожесть с первым символом
+                            s_word += word.charAt(k);
+                        else if (word.charAt(k) == word.charAt(word.length()-1)) //проверка на схожесть с последним символом
+                            s_word += word.charAt(k);
+                        else if(word.charAt(k)==character)
+                            s_word+=word.charAt(k);
+                        else {
+                            s_word += "-"; //заполнение центра слова "-"
+                        }
+                    }
                     flag = 0;
                 }
             }
@@ -48,15 +63,13 @@ public class Main {
                 steps += 1; //приближение к проигрышу
                 System.out.println("Oh no! You on " + steps + "/10 to death");
             } else if (flag == 0) {
-
-                System.out.println("Nice!" + " Now your progress is "+s_word_chars.toString().toLowerCase());
-
+                System.out.println("Nice!" + " Now your progress is "+s_word);
             }
             if (steps == 10)
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    Thread.sleep(100); //создаёт задержку в 1 секунду, сделанно для эффекта
+                    Thread.sleep(500); //создаёт задержку в 1 секунду, сделанно для эффекта
                     System.out.println(".");
                 }
                 System.out.println("Omae wa mou shindeiru!!");
